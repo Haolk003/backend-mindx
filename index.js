ab0b5d3c5848771dcbed0c9aab4ee325a495de72
haolk003 <nguyenquochaolop91@gmail.com>
2023-02-18 21:19:53 +0700

    add

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ app.use("/api/orders", orderRouter);
 app.get("/", (req, res) => {
   res.send("web63 b2");
 });
-const databse = require("./database")
-const db = new databse()
+const databse = require("./database");
+const db = new databse();
 
-const port = 3001;
+const port = "https://example-mindx.onrender.com";
 app.listen(port, () => {
   console.log(`Example app listenting on port ${port}`);
-  db.connect().then((err, result)=>{
-    if(err) throw err
-    console.log('database is connected')
-  })
+  db.connect().then((err, result) => {
+    if (err) throw err;
+    console.log("database is connected");
+  });
 });
